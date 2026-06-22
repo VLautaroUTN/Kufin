a236ea9dc652fee47a6860a563f227cd4388257f
VLautaroUTN <utnlautaro@gmail.com>
2026-06-22 15:22:43 -0300

    hardcode

--- Backend/kufinback/src/app.module.ts
+++ Backend/kufinback/src/app.module.ts
@@ -12,7 +12,7 @@ import { ConfigModule } from '@nestjs/config';
   imports: [GastosModule, TypeOrmModule.forRoot({
     type: 'postgres',
     host: process.env.DB_HOST, // O la URL que nos dé Neon/Supabase
-    port: process.env.DB_PORT,
+    port: 5432,
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
