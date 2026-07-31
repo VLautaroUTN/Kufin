931dcc32a50fe8f95a818337e3ce7f0b1e35314b
VLautaroUTN <utnlautaro@gmail.com>
2026-07-31 07:06:46 -0300

    eliminando comentarios

--- KUfront/src/components/Login/index.tsx
+++ KUfront/src/components/Login/index.tsx
@@ -3,10 +3,9 @@ import Login from './login';
 import axios from 'axios';
 import { useAuth } from '../AuthContext/AuthContext';
 
+
 export interface LoginModalProps {
-  // Indica si el modal debe estar abierto.
   open: boolean;
-  // Función que se ejecutará cuando el modal se cierre.
   onClose: () => void;
 }
 
@@ -16,24 +15,13 @@ export function LoginModal({ open, onClose }: LoginModalProps) {
   const { login } = useAuth();
   const [loading, setLoading] = useState(false);
 
-  // ===================================================
-  // Función que se ejecuta cuando Google autentica
-  // correctamente al usuario.
-  // ===================================================
-  const handleGoogleSuccess = async (respuestaGoogle: any) => {
-
-    // Activa el estado de carga.
-    // Esto normalmente sirve para deshabilitar botones
-    // o mostrar un spinner.
+  const inicoExitoso = async (respuestaGoogle: any) => {
     setLoading(true);
 
     try {
       const API_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000';
       const response = await axios.post( `${API_URL}/auth/google`,
         {
-          // Google devuelve un JWT llamado "credential".
-          // Ese token será enviado al backend para validar
-          // la identidad del usuario.
           token: respuestaGoogle.credential
         }
       );
@@ -48,41 +36,19 @@ export function LoginModal({ open, onClose }: LoginModalProps) {
 
     catch (error) {console.error("Error al autenticar:", error);}
 
-    // finally siempre se ejecuta.
-    // Da igual si hubo éxito o error.
     finally {
-
-      // Desactiva el estado de carga.
       setLoading(false);
-
-      // Cierra el modal.
       onClose();
     }
   };
 
   return (
-
-    // Renderiza el componente Login
     <Login
-
-      // Indica si debe estar abierto.
       open={open}
-
-      // Función para cerrar el modal.
       onClose={onClose}
-
-      // Función que se ejecutará cuando Google
-      // devuelva un login exitoso.
-      onGoogleSuccess={handleGoogleSuccess}
-
-      // Si Google devuelve un error simplemente
-      // lo mostramos por consola.
+      onGoogleSuccess={inicoExitoso}
       onGoogleError={() => console.error("Error en Google")}
-
-      // Indica al componente Login si actualmente
-      // se está realizando una petición al backend.
       loading={loading}
-
     />
   );
 }
